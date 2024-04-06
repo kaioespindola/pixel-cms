371ef63e7eac3ed143b4b7a2de405f47ba819010
Kaio Espindola <kaio.espindola@osf.digital>
2024-04-06 09:20:25 -0400

    Middleware added

---
+++ app/[domain]/page.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return(
+        <div className="container mx-auto h-screen">
+            <h1>Página aqui subdominio</h1>
+        </div>
+    )
+}
+
+export default Login;
---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="container mx-auto h-screen">
+            <h1>Homepage aqui hehe</h1>
+        </div>
+    )
+}
+
+export default Home
